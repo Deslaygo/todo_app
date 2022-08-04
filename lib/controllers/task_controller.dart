15ab84ece3adcc9c6ca73f98f9f95c76f39e5501
javier.delgado <javier.delgado@idtconsultores.com>
2022-08-04 07:46:24 -0500

    [Task] Created method to update task in controller

--- lib/controllers/task_controller.dart
+++ lib/controllers/task_controller.dart
@@ -1,23 +1,35 @@
-import 'dart:convert';
-
 import 'package:get/get.dart';
 import 'package:todo_app/helpers/api_helper.dart';
 import 'package:todo_app/models/task.dart';
 
 class TaskController extends GetxController {
-  final tasks = RxList<Task>([]);
+  final _tasks = RxList<Task>([]);
+
+  List<Task> get tasks => _tasks;
+
+  List<Task> get tasksIncomplete =>
+      tasks.where((ts) => ts.fields?.isCompleted == false).toList();
+
+  List<Task> get tasksCompleted =>
+      tasks.where((ts) => ts.fields?.isCompleted == true).toList();
 
   Future<void> getTasks(Map<String, dynamic> parameters) async {
     try {
-      tasks.value = [];
+      _tasks.value = [];
 
       final data = await ApiHelper.get('tasks', parameters);
 
       if (data != null) {
-        data.forEach((json) => tasks.add(Task.fromJson(json)));
+        data.forEach((json) => _tasks.add(Task.fromJson(json)));
       }
     } catch (e) {
       rethrow;
     }
+  }
+
+  Future updateTask(Task task) async {
+    final index = _tasks.indexWhere((ts) => ts.documentId == task.documentId);
+
+    _tasks[index] = task;
   }
 }
