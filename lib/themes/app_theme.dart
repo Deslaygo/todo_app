6fc256f28a21a12de0d49959b9aeda7cf1deafb3
javier.delgado <javier.delgado@idtconsultores.com>
2022-08-03 22:07:55 -0500

    [Theme] Initialized app theme

--- lib/themes/app_theme.dart
+++ lib/themes/app_theme.dart
@@ -1,1 +1,33 @@
-class AppTheme {}
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class AppTheme {
+  static ThemeData getAppTheme() {
+    return ThemeData(
+      backgroundColor: Colors.white,
+      textTheme: TextTheme(
+        headline1: GoogleFonts?.inter(
+          textStyle: TextStyle(
+            fontSize: 32,
+            fontWeight: FontWeight.w700,
+            color: Color(0XFF292B35),
+          ),
+        ),
+        headline2: GoogleFonts?.inter(
+          textStyle: TextStyle(
+            fontSize: 18,
+            fontWeight: FontWeight.w700,
+            color: Color(0XFF575767),
+          ),
+        ),
+        bodyText1: GoogleFonts?.inter(
+          textStyle: TextStyle(
+            fontSize: 14,
+            fontWeight: FontWeight.w600,
+            color: Color(0XFF575767),
+          ),
+        ),
+      ),
+    );
+  }
+}
