df7535091476550072e69e39c6db0368c3df0e16
javier.delgado <javier.delgado@idtconsultores.com>
2022-08-04 07:47:56 -0500

    [Widgets] Created widget for task item

---
+++ lib/widgest/tasks/task_item.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:todo_app/controllers/task_controller.dart';
+import 'package:todo_app/models/task.dart';
+
+class TaskItem extends StatelessWidget {
+  final Task? task;
+  const TaskItem({Key? key, required this.task}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final taskController = Get.find<TaskController>();
+    return CheckboxListTile(
+      dense: true,
+      controlAffinity: ListTileControlAffinity.leading,
+      checkboxShape:
+          RoundedRectangleBorder(borderRadius: BorderRadius.circular(4)),
+      checkColor: Color(0XFFDADADA),
+      enableFeedback: false,
+      contentPadding: const EdgeInsets.only(left: 0, top: 0, bottom: 0),
+      value: task?.fields?.isCompleted,
+      title: Text(
+        '${task?.fields?.name}',
+        style: Theme.of(context).textTheme.bodyText1,
+      ),
+      subtitle: Row(
+        children: <Widget>[Icon(Icons.money)],
+      ),
+      onChanged: (value) {
+        task?.fields?.isCompleted = value;
+        taskController.updateTask(task!);
+      },
+    );
+  }
+}
--- lib/widgest/widgets.dart
+++ lib/widgest/widgets.dart
@@ -1,1 +1,2 @@
 export 'package:todo_app/widgest/common/custom_progress_indicator.dart';
+export 'package:todo_app/widgest/tasks/task_item.dart';
