ad8297bda2b114a6904e123ec44afb3d6c607a30
javier.delgado <javier.delgado@idtconsultores.com>
2022-08-05 23:51:27 -0500

    [Controlles] Validation is added in methods for task and category

--- lib/controllers/categoy_controller.dart
+++ lib/controllers/categoy_controller.dart
@@ -1,3 +1,30 @@
 import 'package:get/get.dart';
+import 'package:todo_app/controllers/app_controller.dart';
+import 'package:todo_app/helpers/api_helper.dart';
+import 'package:todo_app/models/category.dart';
 
-class CategoryController extends GetxController {}
+class CategoryController extends GetxController {
+  final _categories = RxList<Category>([]);
+
+  List<Category> get categories => _categories;
+
+  Future<void> getCategories(Map<String, dynamic> parameters) async {
+    try {
+      _categories.value = [];
+      final appController = Get.find<AppController>();
+      await appController.validateToken();
+
+      final data = await ApiHelper.get('categories', parameters);
+
+      if (data != null) {
+        data.forEach((json) => _categories.add(Category.fromJson(json)));
+      }
+    } catch (e) {
+      rethrow;
+    }
+  }
+
+  Category? getCategoryById(String id) {
+    return _categories.firstWhereOrNull((ct) => ct.documentId == id);
+  }
+}
--- lib/controllers/task_controller.dart
+++ lib/controllers/task_controller.dart
@@ -1,4 +1,6 @@
 import 'package:get/get.dart';
+import 'package:todo_app/controllers/app_controller.dart';
+import 'package:todo_app/controllers/categoy_controller.dart';
 import 'package:todo_app/helpers/api_helper.dart';
 import 'package:todo_app/models/task.dart';
 
@@ -16,20 +18,55 @@ class TaskController extends GetxController {
   Future<void> getTasks(Map<String, dynamic> parameters) async {
     try {
       _tasks.value = [];
+      final categoryController = Get.find<CategoryController>();
+      final appController = Get.find<AppController>();
+      await appController.validateToken();
+
+      await categoryController.getCategories(parameters);
 
       final data = await ApiHelper.get('tasks', parameters);
 
       if (data != null) {
-        data.forEach((json) => _tasks.add(Task.fromJson(json)));
+        data.forEach((json) {
+          final categoryId =
+              json['fields']['categoryId']['stringValue'].toString();
+          final category = categoryController.getCategoryById(categoryId);
+          json['category'] = category?.toJson();
+          final task = Task.fromJson(json);
+          _tasks.add(task);
+        });
       }
     } catch (e) {
+      print(e.toString());
       rethrow;
     }
   }
 
   Future updateTask(Task task) async {
+    final appController = Get.find<AppController>();
+    await appController.validateToken();
     final index = _tasks.indexWhere((ts) => ts.documentId == task.documentId);
 
+    final data = <String, dynamic>{};
+
+    data['date'] = {'integerValue': task.fields?.date};
+    data['categoryId'] = {'stringValue': task.fields?.categoryId};
+    data['name'] = {'stringValue': task.fields?.name};
+    data['isCompleted'] = {'booleanValue': task.fields?.isCompleted};
+
+    final parameters = <String, dynamic>{
+      'fields': data,
+    };
+
+    await ApiHelper.patch('tasks', task.documentId!, parameters);
+
     _tasks[index] = task;
+  }
+
+  Future addTask(Map<String, dynamic> parameters) async {
+    final appController = Get.find<AppController>();
+    await appController.validateToken();
+    await ApiHelper.post('tasks', parameters);
+    await getTasks(<String, dynamic>{});
   }
 }
