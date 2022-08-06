d5e9636451d41b4f3fad8ce0c265e3343f212528
javier.delgado <javier.delgado@idtconsultores.com>
2022-08-05 23:54:05 -0500

    [Themes] Colors file was added

--- lib/themes/app_theme.dart
+++ lib/themes/app_theme.dart
@@ -1,30 +1,43 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:todo_app/themes/color_palette.dart';
 
 class AppTheme {
   static ThemeData getAppTheme() {
     return ThemeData(
-      backgroundColor: Colors.white,
+      backgroundColor: backgroundColor,
+      primaryColor: primaryColor,
+      appBarTheme: AppBarTheme(
+        elevation: 0,
+        backgroundColor: backgroundColor,
+      ),
       textTheme: TextTheme(
         headline1: GoogleFonts?.inter(
           textStyle: TextStyle(
             fontSize: 32,
             fontWeight: FontWeight.w700,
-            color: Color(0XFF292B35),
+            color: titleColor,
           ),
         ),
         headline2: GoogleFonts?.inter(
           textStyle: TextStyle(
             fontSize: 18,
             fontWeight: FontWeight.w700,
-            color: Color(0XFF575767),
+            color: textColor,
           ),
         ),
         bodyText1: GoogleFonts?.inter(
           textStyle: TextStyle(
             fontSize: 14,
             fontWeight: FontWeight.w600,
-            color: Color(0XFF575767),
+            color: textColor,
+          ),
+        ),
+        bodyText2: GoogleFonts?.inter(
+          textStyle: TextStyle(
+            fontSize: 12,
+            fontWeight: FontWeight.w600,
+            color: textColor,
           ),
         ),
       ),
---
+++ lib/themes/color_palette.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/widgets.dart';
+
+const Color primaryColor = Color(0xff1294F2);
+const Color backgroundColor = Color(0XFFffffff);
+
+const Color titleColor = Color(0XFF292B35);
+const Color textColor = Color(0XFF575767);
+const Color inputColor = Color(0xffF8F8F8);
+
+const Color errorColor = Color(0XFFe74c3c);
+const Color successColor = Color(0XFF27ae60);
